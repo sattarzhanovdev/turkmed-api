a393b1b8ef21662177129ccb0058b0ada55133b3
sattarzhanovdev <sattarzhanovdev@gmail.com>
2023-12-21 13:42:40 +0600

    push

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,8 @@
- echo "BUILD START"
- python3.9 -m pip install -r req.txt
- python3.9 -m pip install django
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+echo "BUILD START"
+python3.9 -m pip install -r req.txt
+
+python3.9 manage.py makemigrations --noinput
+python3.9 manage.py migrate --noinput
+
+python3.9 manage.py collectstatic --noinput --clear
+echo "BUILD END"
--- core/settings.py
+++ core/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
+import dj_database_url
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -114,6 +116,8 @@ DATABASES = {
 #     }
 # }
 
+DATABASES['default'] = dj_database_url.config()
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
@@ -149,8 +153,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles_build' / 'static'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 
 MEDIA_URLS ='/media/'
