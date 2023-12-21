4316a0b619511fdee9dc6bebb7d316ff8229ac9b
sattarzhanovdev <sattarzhanovdev@gmail.com>
2023-12-21 12:27:16 +0600

    push

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,6 @@
  echo "BUILD START"
  python3.9 -m pip install -r req.txt
  python3.9 -m pip install django
+ python3.9 -m pip install whitenoise
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
