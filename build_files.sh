9d1bdeb715488da51972c420f0b925392594ea99
sattarzhanovdev <sattarzhanovdev@gmail.com>
2023-12-21 13:21:23 +0600

    push

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,5 @@
  echo "BUILD START"
  python3.9 -m pip install -r req.txt
  python3.9 -m pip install django
- python3.9 -m pip install whitenoise
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
--- core/settings.py
+++ core/settings.py
@@ -97,22 +97,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
-}
-
-DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
-        'USER': 'postgres',
-        'PASSWORD': '22092008god',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': '22092008god',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
