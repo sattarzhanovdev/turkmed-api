d686465c987e9202f7a74c9c19a70d43abef46ab
sattarzhanovdev <sattarzhanovdev@gmail.com>
2023-12-21 12:40:28 +0600

    pus

--- build_files.sh
+++ build_files.sh
@@ -2,5 +2,6 @@
  python3.9 -m pip install -r req.txt
  python3.9 -m pip install django
  python3.9 -m pip install whitenoise
+ python3.9 -m pip install _sqlite3
  python3.9 manage.py collectstatic --noinput --clear
  echo "BUILD END"
