3404bbc60fbaa846d20db88bbcc6cd732fb318f5
sattarzhanovdev <sattarzhanovdev@gmail.com>
2023-12-21 12:33:18 +0600

    pus

--- core/urls.py
+++ core/urls.py
@@ -49,10 +49,10 @@ urlpatterns = [
     path('appointment/', include('apps.appointment.urls')),
     path('reviews/', include('apps.reviews.urls')),
     #SWAGGER
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    # path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    # path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
