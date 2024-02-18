a3402017d03e49e05aebc3ada430ca438419dae5
sattarzhanovdev <sattarzhanovdev@gmail.com>
2024-02-18 23:16:18 +0600

    push

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,2 @@
- echo "BUILD START"
- python3.9 -m pip install -r req.txt
- python3.9 manage.py collectstatic --noinput --clear
- echo "BUILD END"
+pip install -r req.txt 
+python3.9 manage.py collectstatic
